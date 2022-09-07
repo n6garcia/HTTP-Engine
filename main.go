6d126f5ce48b35c352a99441b6833b845b331c2d
Noel Garcia <n6garcia@ucsd.edu>
2022-09-06 19:01:15 -0700

    building out server

--- main.go
+++ main.go
@@ -1,14 +1,94 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"log"
 	"net"
-	"net/http"
+	"strings"
 )
+
+type Request struct {
+	method string
+	route  string
+
+	// Map string->string header->value
+}
 
 func main() {
 
-	l, _ := net.Listen("tcp", ":8080")
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
-	http.Serve(l, nil)
+	defer li.Close()
 
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		go handle(conn)
+	}
+
+}
+
+func handle(con net.Conn) {
+	defer con.Close()
+
+	req := request(con)
+
+	respond(con, req)
+
+}
+
+func request(con net.Conn) Request {
+	req := Request{}
+
+	i := 0
+	scanner := bufio.NewScanner(con)
+	for scanner.Scan() {
+		ln := scanner.Text()
+
+		if ln == "" {
+			break
+		}
+
+		if i == 0 {
+			fields := strings.Fields(ln)
+			req.method = fields[0]
+			req.route = fields[1]
+		} else {
+			strs := strings.Split(ln, ":")
+			fmt.Println(strs)
+		}
+
+		i++
+	}
+
+	/* scan optional HTTP Body
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if ln == "" {
+			break
+		}
+		i++
+	}*/
+
+	return req
+}
+
+func respond(con net.Conn, req Request) {
+	body := `<b>Hello!</b>`
+
+	fmt.Fprint(con, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(con, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(con, "Content-Type: text/html\r\n")
+	fmt.Fprint(con, "\r\n")
+	fmt.Fprint(con, body)
 }
