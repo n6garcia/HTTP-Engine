224df720f6c68191aaf72ff27100bc4f18fd15ed
Noel Garcia <n6garcia@ucsd.edu>
2022-09-06 21:10:01 -0700

    setup basic http for favicon.ico

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"strconv"
 	"strings"
 )
 
@@ -12,7 +14,16 @@ type Request struct {
 	method string
 	route  string
 
-	// Map string->string header->value
+	headers map[string]string
+}
+
+type Response struct {
+	code int
+	desc string
+
+	headers map[string]string
+
+	body []byte
 }
 
 func main() {
@@ -47,12 +58,13 @@ func handle(con net.Conn) {
 }
 
 func request(con net.Conn) Request {
-	req := Request{}
+	req := Request{headers: make(map[string]string)}
 
 	i := 0
 	scanner := bufio.NewScanner(con)
 	for scanner.Scan() {
 		ln := scanner.Text()
+		fmt.Println(ln)
 
 		if ln == "" {
 			break
@@ -63,32 +75,68 @@ func request(con net.Conn) Request {
 			req.method = fields[0]
 			req.route = fields[1]
 		} else {
-			strs := strings.Split(ln, ":")
-			fmt.Println(strs)
+			strs := strings.SplitN(ln, ":", 2)
+			if len(strs) != 2 {
+				break
+			}
+
+			req.headers[strs[0]] = strs[1]
+
 		}
 
 		i++
 	}
 
-	/* scan optional HTTP Body
-	for scanner.Scan() {
-		ln := scanner.Text()
-		fmt.Println(ln)
-		if ln == "" {
-			break
-		}
-		i++
-	}*/
+	/*
+		for key, val := range req.headers {
+			fmt.Println(key, " ", val)
+		}*/
 
 	return req
 }
 
 func respond(con net.Conn, req Request) {
-	body := `<b>Hello!</b>`
 
-	fmt.Fprint(con, "HTTP/1.1 200 OK\r\n")
-	fmt.Fprintf(con, "Content-Length: %d\r\n", len(body))
-	fmt.Fprint(con, "Content-Type: text/html\r\n")
-	fmt.Fprint(con, "\r\n")
-	fmt.Fprint(con, body)
+	// if route is special "index.html"
+	if req.route == "/" {
+		body := `<b>Hello!</b>`
+		fmt.Fprint(con, "HTTP/1.1 200 OK\r\n")
+		fmt.Fprintf(con, "Content-Length: %d\r\n", len(body))
+		fmt.Fprint(con, "Content-Type: text/html\r\n")
+		fmt.Fprint(con, "\r\n")
+		fmt.Fprint(con, body)
+	} else {
+
+		res := Response{headers: make(map[string]string)}
+
+		res.code = 200
+		res.desc = "OK"
+
+		bs, err := os.ReadFile("./public_html/favicon.ico")
+		if err != nil {
+			panic(err)
+		}
+
+		res.body = bs
+		res.headers["Content-Length"] = strconv.Itoa(len(bs))
+		res.headers["Content-Type"] = "image/vnd.microsoft.icon"
+
+		res.Write(con)
+
+	}
+
+}
+
+func (res Response) Write(con net.Conn) {
+
+	fmt.Fprintf(con, "HTTP/1.1 %d %s\r\n", res.code, res.desc)
+	fmt.Printf("HTTP/1.1 %d %s\r\n", res.code, res.desc)
+	for key, val := range res.headers {
+		fmt.Fprintf(con, "%s: %s\r\n", key, val)
+		fmt.Printf("%s: %s\r\n", key, val)
+	}
+	fmt.Fprintf(con, "\r\n")
+	fmt.Printf("\r\n")
+	con.Write(res.body)
+
 }
